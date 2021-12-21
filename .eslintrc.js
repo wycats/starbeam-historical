625c0f0aa08a1b01fe8eeb117867cefbeea463bb
Chirag Patel <chpatel@linkedin.com>
2021-12-20 16:53:11 -0800

    Add linting and make it run on CI

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint",
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  rules: {
+    "prefer-const": "off"
+  },
+};
